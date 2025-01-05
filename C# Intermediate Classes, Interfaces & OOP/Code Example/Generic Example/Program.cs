cefe07a31d197b2fb1e9ab9024d2da672b0c28c3
Md. Rifatul Islam <rifatuul@gmail.com>
2025-01-05 19:27:35 +0600

    Generic

---
+++ C# Intermediate Classes, Interfaces & OOP/Code Example/Generic Example/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+
+namespace Generic_Example
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var lists = new List<int>();
+            lists.Add(1);
+            lists.Add(2);
+            lists.Add(3);
+            lists.Add(4);
+            lists.Add(5);
+
+            foreach (var list in lists)
+                Console.WriteLine(list);
+
+        }
+    }
+}
