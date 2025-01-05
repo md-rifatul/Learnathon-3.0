f5c7c77dec76101fbbab7243ebee58f90af49e27
Md. Rifatul Islam <rifatuul@gmail.com>
2025-01-05 19:19:39 +0600

    Boxing and Unboxing

---
+++ C# Intermediate Classes, Interfaces & OOP/Code Example/Boxing & Unboxing/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Boxing___Unboxing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Boxing
+            var number = 10;
+            object obj = number;
+            Console.WriteLine(obj);
+
+            //Unboxing
+            object obj1 = 50;
+            var number2 = (int)obj1;
+            Console.WriteLine(number2);
+
+        }
+    }
+}
