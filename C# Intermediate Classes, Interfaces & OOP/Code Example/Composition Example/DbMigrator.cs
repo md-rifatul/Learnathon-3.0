2ad0a6ed247fe45ff7edaa795f7a414f2867697f
Md. Rifatul Islam <rifatuul@gmail.com>
2025-01-05 16:06:41 +0600

    Composition

---
+++ C# Intermediate Classes, Interfaces & OOP/Code Example/Composition Example/DbMigrator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Composition_Example
+{
+    public class DbMigrator
+    {
+        private readonly Logger _logger;
+
+        public DbMigrator(Logger logger)
+        {
+            this._logger = logger;
+        }
+        public void Migrate()
+        {
+            _logger.Log("We are migrating ...");
+        }
+    }
+}
