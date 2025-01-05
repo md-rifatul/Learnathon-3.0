6fcbb6088d78d17fea497f4653616d41a4cd43d2
Md. Rifatul Islam <rifatuul@gmail.com>
2025-01-05 19:23:18 +0600

    NonGeneric

---
+++ C# Intermediate Classes, Interfaces & OOP/Code Example/NonGeneric Example/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+
+namespace NonGeneric_Example
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList arrayList = new ArrayList();
+            arrayList.Add(1);
+            arrayList.Add("Two");
+            arrayList.Add(2.56);
+            arrayList.Add(new DateTime(2015,5,15));
+
+            foreach (var array in arrayList)
+                Console.WriteLine(array);
+
+        }
+    }
+}
