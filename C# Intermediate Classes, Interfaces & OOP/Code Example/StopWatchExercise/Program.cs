ddb6f413d818eac471920258024b59fad61c8cef
Md. Rifatul Islam <rifatuul@gmail.com>
2025-01-06 18:19:34 +0600

    Stop Watch Exercise

---
+++ C# Intermediate Classes, Interfaces & OOP/Code Example/StopWatchExercise/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading;
+
+namespace StopWatchExercise
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var stopWatch = new StopWatch();
+
+            for (var i = 0; i < 2; i++)
+            {
+                stopWatch.Start();
+                Thread.Sleep(1000);
+                stopWatch.Stop();
+
+                Console.WriteLine("Duration: "+stopWatch.GetInterval());
+                Console.WriteLine("Press Enter to run the stopwatch one more time");
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ C# Intermediate Classes, Interfaces & OOP/Code Example/StopWatchExercise/StopWatch.cs
@@ -0,0 +1,31 @@
+﻿namespace StopWatchExercise
+{
+    public class StopWatch
+    {
+        private DateTime _startTime;
+        private DateTime _endTime;
+        private bool _running;
+        
+        public void Start()
+        {
+            if(_running)
+                throw new InvalidOperationException("StopWatch is already running!");
+
+            _startTime = DateTime.Now;
+            _running = true;
+        }
+
+        public void Stop()
+        {
+            if (!_running)
+                throw new InvalidOperationException("Stopwatch is not running!");
+            _endTime = DateTime.Now;
+            _running = false;
+        }
+        public TimeSpan GetInterval()
+        {
+            return _endTime - _startTime;
+        }
+    }
+
+}
