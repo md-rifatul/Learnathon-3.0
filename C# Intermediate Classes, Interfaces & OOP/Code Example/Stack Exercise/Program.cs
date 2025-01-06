3467773c2836a8e81f93a3b6a31129c47fffb150
Md. Rifatul Islam <rifatuul@gmail.com>
2025-01-06 18:39:05 +0600

    Stack

---
+++ C# Intermediate Classes, Interfaces & OOP/Code Example/Stack Exercise/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+
+namespace Stack_Exercise
+{
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack _stack = new Stack();
+
+            int ini = 1;
+            int max = 5;
+
+            for (int i = ini; i <= max; i++)
+                _stack.Push(i);
+
+            for (int i = ini; i <= max; i++)
+                Console.WriteLine(_stack.Pop());
+
+            Console.WriteLine();
+        }
+    }
+}
---
+++ C# Intermediate Classes, Interfaces & OOP/Code Example/Stack Exercise/Stack.cs
@@ -0,0 +1,41 @@
+﻿namespace Stack_Exercise
+{
+    public class Stack
+    {
+        private List<object> _list = new List<object>();
+
+        public void Push(object obj)
+        {
+            if (obj == null)
+                throw new InvalidOperationException("You can't add a null object to the stack");
+
+            _list.Add(obj);
+            int elements = _list.Count;
+
+            if (elements > 1)
+            {
+                for (int i = elements - 1; i > 0; i--) 
+                {
+                    _list[i] = _list[i - 1];
+                }
+                _list[0] = obj;
+            }
+        }
+
+        public object Pop()
+        {
+            if (_list.Count == 0) 
+                throw new InvalidOperationException("There are no elements in the stack yet");
+
+            object ToReturn = _list[0];
+
+            int elements = _list.Count;
+            for (int i = 0; i < elements - 1; i++)
+            {
+                _list[i] = _list[i + 1];
+            }
+            _list.RemoveAt(elements - 1);
+            return ToReturn;
+        }
+    }
+}
