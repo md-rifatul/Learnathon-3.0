61a8c9a49bab3d765c933b551066d9c6fd5d796c
Md. Rifatul Islam <rifatuul@gmail.com>
2025-01-07 13:19:40 +0600

    Extension Method

---
+++ C# Advanced Topics/Code Example/ExtensionMethods/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace ExtensionMethods
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string post = "This is supposed to be a very long .....";
+            var sortenedPost = post.Shorten(5);
+
+            Console.WriteLine(sortenedPost);
+        }
+    }
+}
