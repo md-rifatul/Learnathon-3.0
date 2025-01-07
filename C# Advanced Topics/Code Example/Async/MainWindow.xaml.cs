47d4e487bed195a8c50f178a41cecbe7f4240163
Md. Rifatul Islam <rifatuul@gmail.com>
2025-01-07 21:39:37 +0600

    Asynchronous programming

---
+++ C# Advanced Topics/Code Example/Async/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System.IO;
+using System.Net;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Async
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            DownloadHtml("http://msdn.microsoft.com");
+        }
+
+        private void DownloadHtml(string v)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Download (string url)
+        {
+            var webClient = new WebClient();
+            var html = webClient.DownloadString(url);
+
+            using (var streamWriter = new StreamWriter(@"c:\project\result.html"))
+            {
+                streamWriter.Write(html);
+            }
+        }
+    }
+}
