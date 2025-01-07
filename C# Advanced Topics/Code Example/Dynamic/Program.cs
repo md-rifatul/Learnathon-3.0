9377832b0ba459b10d515271b11daee20de358c2
Md. Rifatul Islam <rifatuul@gmail.com>
2025-01-07 18:29:38 +0600

    Dynamic

---
+++ C# Advanced Topics/Code Example/Dynamic/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Dynamic
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //object obj = "rifatul";
+
+            //var methodInfo = obj.GetType().GetMethod("GetHashCode");
+            //methodInfo.Invoke(null, null);
+
+            //dynamic excelObject = "rifatul";
+            //excelObject.Optimize();
+
+            object name = "Rifatul";
+            name = 10;
+            Console.WriteLine(name);
+
+            dynamic a = 10;
+            dynamic b = 20;
+            var c = a + b;
+            Console.WriteLine(c);
+        }
+    }
+}
